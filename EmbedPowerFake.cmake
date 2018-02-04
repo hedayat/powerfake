559d004e2122e9c9fe6eae2b2dcc95c6a2464388
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2018-02-05 01:15:06 +0330

    Some cleanups in CMake files. User no longer needs to explicitly link powerfake library to its target

--- EmbedPowerFake.cmake
+++ EmbedPowerFake.cmake
@@ -8,6 +8,7 @@ include(${POWERFAKE_DIR}/cmake/PowerFakeFunctions.cmake)
 
 add_library(powerfake STATIC ${POWERFAKE_DIR}/powerfake.cpp
     ${POWERFAKE_DIR}/powerfake.h)
+add_library(PowerFake::powerfake ALIAS powerfake)
 
 set(pair_sources ${POWERFAKE_DIR}/powerfake ${POWERFAKE_DIR}/SymbolAliasMap
     ${POWERFAKE_DIR}/NMSymbolReader ${POWERFAKE_DIR}/Reader)
@@ -17,6 +18,7 @@ set(powerfake_headers $<JOIN:${pair_sources},.h >)
 
 add_library(pw_bindfakes STATIC ${powerfake_sources} ${powerfake_headers})
 set_property(TARGET pw_bindfakes APPEND PROPERTY COMPILE_DEFINITIONS BIND_FAKES)
+add_library(PowerFake::pw_bindfakes ALIAS pw_bindfakes)
 
 if (NOT ${PROJECT_NAME} STREQUAL "powerfake")
     file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/dummy.cpp "")
@@ -25,5 +27,5 @@ if (NOT ${PROJECT_NAME} STREQUAL "powerfake")
     # don't use
     set_property(TARGET bind_fakes APPEND PROPERTY
         LINK_FLAGS "-Wl,--gc-sections")
-    target_link_libraries(bind_fakes pw_bindfakes)
+    target_link_libraries(bind_fakes PowerFake::pw_bindfakes)
 endif (NOT ${PROJECT_NAME} STREQUAL "powerfake")
--- cmake/PowerFakeFunctions.cmake
+++ cmake/PowerFakeFunctions.cmake
@@ -1,10 +1,7 @@
 function(bind_fakes target_name test_lib wrapper_funcs_lib)
-    if (NOT BIND_FAKES_LINKED)
-        set(BIND_FAKES_LINKED 1 PARENT_SCOPE)
-        target_link_libraries(bind_fakes pw_bindfakes
-            -Wl,--whole-archive ${wrapper_funcs_lib} -Wl,--no-whole-archive
-            $<TARGET_PROPERTY:${target_name},LINK_LIBRARIES>)
-    endif (NOT BIND_FAKES_LINKED)
+    target_link_libraries(bind_fakes
+        -Wl,--whole-archive ${wrapper_funcs_lib} -Wl,--no-whole-archive
+        $<TARGET_PROPERTY:${target_name},LINK_LIBRARIES>)
 
     add_custom_command(TARGET ${target_name} PRE_LINK
         COMMAND bind_fakes $<TARGET_FILE:${test_lib}>
@@ -13,4 +10,5 @@ function(bind_fakes target_name test_lib wrapper_funcs_lib)
     # Add powerfake link flags
     set_property(TARGET ${target_name} APPEND_STRING PROPERTY
         LINK_FLAGS @${CMAKE_CURRENT_BINARY_DIR}/powerfake.link_flags)
+    target_link_libraries(${target_name} PowerFake::powerfake)
 endfunction(bind_fakes)
