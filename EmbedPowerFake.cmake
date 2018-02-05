6ef19dee9bff5575b52a20e37ef8363909b17a94
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2018-02-05 13:21:39 +0330

    Generate a separate bind_fakes_ executable for each target used in a bind_fakes() CMake function call

--- EmbedPowerFake.cmake
+++ EmbedPowerFake.cmake
@@ -19,13 +19,3 @@ set(powerfake_headers $<JOIN:${pair_sources},.h >)
 add_library(pw_bindfakes STATIC ${powerfake_sources} ${powerfake_headers})
 set_property(TARGET pw_bindfakes APPEND PROPERTY COMPILE_DEFINITIONS BIND_FAKES)
 add_library(PowerFake::pw_bindfakes ALIAS pw_bindfakes)
-
-if (NOT ${PROJECT_NAME} STREQUAL "powerfake")
-    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/dummy.cpp "")
-    add_executable(bind_fakes ${CMAKE_CURRENT_BINARY_DIR}/dummy.cpp)
-    # Add flags to ignore undefined reference to __real_ function calls, which we
-    # don't use
-    set_property(TARGET bind_fakes APPEND PROPERTY
-        LINK_FLAGS "-Wl,--gc-sections")
-    target_link_libraries(bind_fakes PowerFake::pw_bindfakes)
-endif (NOT ${PROJECT_NAME} STREQUAL "powerfake")
--- cmake/PowerFakeFunctions.cmake
+++ cmake/PowerFakeFunctions.cmake
@@ -1,10 +1,18 @@
 function(bind_fakes target_name test_lib wrapper_funcs_lib)
-    target_link_libraries(bind_fakes
+    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/dummy.cpp "")
+    set(bind_fakes_tgt bind_faked_${target_name})
+    add_executable(${bind_fakes_tgt} EXCLUDE_FROM_ALL
+        ${CMAKE_CURRENT_BINARY_DIR}/dummy.cpp)
+    # Remove __real_ & __wrap_ functions to prevent undefined reference errors
+    # These are not used by bind_fakes
+    set_property(TARGET ${bind_fakes_tgt} APPEND PROPERTY
+        LINK_FLAGS "-Wl,--gc-sections")
+    target_link_libraries(${bind_fakes_tgt} PowerFake::pw_bindfakes
         -Wl,--whole-archive ${wrapper_funcs_lib} -Wl,--no-whole-archive
         $<TARGET_PROPERTY:${target_name},LINK_LIBRARIES>)
 
     add_custom_command(TARGET ${target_name} PRE_LINK
-        COMMAND bind_fakes $<TARGET_FILE:${test_lib}>
+        COMMAND ${bind_fakes_tgt} $<TARGET_FILE:${test_lib}>
                 $<TARGET_FILE:${wrapper_funcs_lib}>)
 
     # Add powerfake link flags
