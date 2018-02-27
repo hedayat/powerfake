90ad67d52fcf4f8c1dd4b82c0b2974a99e1ef15f
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2018-02-28 02:35:38 +0330

    Small cleanup in emplace() call

--- fakeit/powerfakeit.h
+++ fakeit/powerfakeit.h
@@ -71,7 +71,7 @@ class PowerFakeIt: public fakeit::ActualInvocationsSource
                     [recorder](Args... args) {
                         recorder->handleMethodInvocation(args...);
                     }) );
-                auto ins = mocked.emplace(std::make_pair(f_key, FakeData(fake_ptr, recorder)));
+                auto ins = mocked.emplace(f_key, FakeData(fake_ptr, recorder));
                 mocked_it = ins.first;
             }
             return fakeit::MockingContext<R, Args...>(
@@ -91,7 +91,7 @@ class PowerFakeIt: public fakeit::ActualInvocationsSource
                     [recorder](Args... args) {
                         return recorder->handleMethodInvocation(args...);
                     }) );
-                auto ins = mocked.emplace(std::make_pair(f_key, FakeData(fake_ptr, recorder)));
+                auto ins = mocked.emplace(f_key, FakeData(fake_ptr, recorder));
                 mocked_it = ins.first;
             }
             return fakeit::MockingContext<R, Args...>(
